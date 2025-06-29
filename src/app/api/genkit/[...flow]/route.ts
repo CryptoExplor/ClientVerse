a8c0d4a83c2a6d029df41add5e8ae70dd2654807
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2025-06-30 01:40:39 +0530

    Update route.ts

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,11 +1,1 @@
-'use server';
 
-import { createApiHandler } from '@genkit-ai/next';
-
-// This is the entry point for all of your Genkit flows in production.
-// It's important to import all of your flows here so that they are
-// registered with Genkit.
-import '@/ai/flows/product-recommendations';
-import '@/ai/flows/data-autofill';
-
-export const { GET, POST, OPTIONS } = createApiHandler();
