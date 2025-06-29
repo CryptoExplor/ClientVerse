4ebc8049ddd7a62cfea1809108936c9a0d32fd4c
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2025-06-30 01:41:13 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,1 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase';
-import {next} from '@genkit-ai/next';
 
-export const ai = genkit({
-  plugins: [googleAI(), firebase(), next()],
-  model: 'googleai/gemini-2.0-flash',
-});
